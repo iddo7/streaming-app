026d01fd7c3b87684a077399ae63895da50bc524
iddo07 <isaacnegreiros10@gmail.com>
2023-10-19 17:05:33 -0400

    Enabling movies seeding

--- Netflix/database/seeders/DatabaseSeeder.php
+++ Netflix/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(MoviesSeeder::class);
         $this->call(PersonsSeeder::class);
     }
 }
