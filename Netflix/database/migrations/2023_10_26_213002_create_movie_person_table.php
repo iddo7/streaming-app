c6e0dc5cb424069ddf142bcc62f04e864da6b9ba
iddo07 <isaacnegreiros10@gmail.com>
2023-10-26 17:31:43 -0400

    Renaming joint table to fit convention

--- Netflix/database/migrations/2023_10_26_213002_create_movie_person_table.php
+++ Netflix/database/migrations/2023_10_26_213002_create_movie_person_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('movies_persons', function (Blueprint $table) {
+        Schema::create('movie_person', function (Blueprint $table) {
             $table->id();
             $table->foreignId('movie_id')->contrained();
             $table->foreignId('person_id')->contrained();
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('movies_persons');
+        Schema::dropIfExists('movie_person');
     }
 };
