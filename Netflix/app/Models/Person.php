e9fa0fbf70c9a23e570e3dfd0b796a2eb01c4731
iddo07 <isaacnegreiros10@gmail.com>
2023-10-26 17:10:21 -0400

    Creating relation persons

--- Netflix/app/Models/Person.php
+++ Netflix/app/Models/Person.php
@@ -9,4 +9,9 @@ class Person extends Model
 {
     protected $table = 'persons';
     use HasFactory;
+
+    public function movies() : HasMany
+    {
+        return $this->belongsToMany(Movie::class);
+    }
 }
