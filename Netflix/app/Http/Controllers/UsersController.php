402268424b00c99c8cbca7586a2af927427eebbf
iddo07 <isaacnegreiros10@gmail.com>
2023-11-16 16:41:02 -0500

    Login function

--- Netflix/app/Http/Controllers/UsersController.php
+++ Netflix/app/Http/Controllers/UsersController.php
@@ -6,18 +6,23 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function index() 
+    public function loginPage() 
     {
-
+        return View('Users.loginPage');
     }
 
-    public function login()
+    public function login(Request $request)
     {
+        $success = Auth::attempt(['email' => $request->email, 'password' => $request->password]);
 
+        if ($success)
+            return redirect()->route('movies.index')->with('messages', "Connected successfully");
+        else
+            return redirect()->route('login')->withErrors(['Invalid username or password']);
     }
 
     public function logout()
     {
-        
+
     }
 }
