980a9abc40d6b0cb2ca5ad7ffe8fd8e77d7f0a5b
iddo07 <isaacnegreiros10@gmail.com>
2023-11-16 16:02:37 -0500

    Creating Users Controller

---
+++ Netflix/app/Http/Controllers/UsersController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function index() 
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function logout()
+    {
+        
+    }
+}
