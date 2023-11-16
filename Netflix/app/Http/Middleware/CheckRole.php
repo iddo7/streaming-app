794e3919839af0a7615517f14e657ea4f0f5f485
iddo07 <isaacnegreiros10@gmail.com>
2023-11-16 17:29:07 -0500

    Creating Middleware

---
+++ Netflix/app/Http/Middleware/CheckRole.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+        $user = $request->user();
+
+        if (!$user) {
+            if (!$request->is('login')) {
+                return redirect()->route('login');
+            }
+        }
+        else {
+            if (!in_array($user->role, $roles)) {
+                return redirect()->route('login');
+            }
+        }
+
+        return $next($request);
+    }
+}
